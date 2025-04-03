606913fbf0fff8aceef6a7e1c0bc1d72d6ab424f
Untung Budiman <untungbudiman.dev@gmail.com>
2025-04-03 19:52:11 +0700

    create input and loader spinner

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,119 @@
 "use client"
 
 import AppHeader from '@/components/app-header'
+import Spinner from '@/components/spinner'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import MainLayout from '@/layouts/main-layout'
 import { SetTitle } from '@/lib/setHelmet'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function DashboardPage() {
+  const [state, setState] = useState({
+    loading: true,
+    page_loading: false,
+    nama_sekolah: '',
+    alamat_sekolah: '',
+    no_telp: '',
+    email: '',
+    website: '',
+    status_sekolah: '',
+    type_sekolah: '',
+    npsn: '',
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async () => {
+    setState(prev => ({ ...prev, loading: true }))
+    console.log(state)
+  }
+
+  if (state.page_loading) {
+    return (
+      <MainLayout>
+        <SetTitle pageTitle='Dashboard' />
+        <div className='w-full h-full flex items-center justify-center'>
+          <Spinner />
+        </div>
+      </MainLayout>
+    )
+  }
+
   return (
     <MainLayout>
       <SetTitle pageTitle='Dashboard' />
-      <AppHeader title='Selamat Datang Kembali, Admin!'/>
-      <div></div>
+      <AppHeader title='Selamat Datang Kembali, Admin!' />
+      <div className='space-y-3'>
+        <div className='grid grid-cols-5 gap-4'>
+
+          <div className='form-group col-span-2'>
+            <Label htmlFor='nama_sekolah'>Nama Sekolah</Label>
+            <Input name='nama_sekolah' id='nama_sekolah' type='text' value={state.nama_sekolah} onChange={handleChange} placeholder='Ketikan nama sekolah' />
+          </div>
+
+          <div className='form-group'>
+            <Label htmlFor='npsn'>NPSN Sekolah</Label>
+            <Input name='npsn' id='npsn' type='number' value={state.npsn} onChange={handleChange} placeholder='Ketikan NPSN sekolah' />
+          </div>
+
+          <div className='form-group'>
+            <Label htmlFor='status_sekolah'>Status Sekolah</Label>
+            <Select onValueChange={(value) => setState({ ...state, status_sekolah: value })}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Status Sekolah" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="NEGERI">Negeri</SelectItem>
+                <SelectItem value="SWASTA">Swasta</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='form-group'>
+            <Label>Tipe Sekolah</Label>
+            <Select onValueChange={(value) => setState({ ...state, type_sekolah: value })}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Tipe Sekolah" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="SMA">SMA</SelectItem>
+                <SelectItem value="SMK">SMK</SelectItem>
+                <SelectItem value="MA">MA</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+        </div>
+        <div className='grid grid-cols-3 gap-4'>
+          <div className='form-group'>
+            <Label htmlFor='email'>Email Sekolah</Label>
+            <Input name='email' id='email' type='email' onChange={handleChange} value={state.email} placeholder='Ketikan email sekolah' />
+          </div>
+          <div className='form-group'>
+            <Label htmlFor='website'>Website Sekolah</Label>
+            <Input name='website' id='website' type="url" onChange={handleChange} value={state.website} placeholder='Ketikan website sekolah' />
+          </div>
+          <div className='form-group'>
+            <Label htmlFor='no_telp'>Nomor Telephone Sekolah</Label>
+            <Input name='no_telp' id='no_telp' type="tel" onChange={handleChange} value={state.no_telp} placeholder='Ketikan nomor telepone sekolah' />
+          </div>
+        </div>
+        <div className='form-group'>
+          <Label htmlFor='alamat_sekolah'>Alamat Sekolah</Label>
+          <Input name='alamat_sekolah' type="text" placeholder='Ketikan alamat lengkap sekolah disini...' value={state.alamat_sekolah} onChange={handleChange} />
+        </div>
+        <div className='flex items-center justify-end'>
+          <Button onClick={handleSubmit}>{state.loading ? "Menyimpan Data" : "Simpan Data"}</Button>
+        </div>
+      </div>
     </MainLayout>
   )
 }
---
+++ components/spinner.tsx
@@ -0,0 +1,38 @@
+import { useTheme } from 'next-themes'
+import { SyncLoader } from 'react-spinners'
+
+interface Props {
+    size?: "large" | "medium" | "small" | "extraSmall" | "extraLarge"
+    color?: "white" | "black" | "auto" | "background"
+    label?: string
+}
+
+export default function Spinner({ size = "medium", color = "auto", label }: Props) {
+    const { theme } = useTheme()
+
+    const colorVariants = {
+        white: "#ffffff",
+        black: "#000000",
+        auto: theme === "dark" ? "#ffffff" : "#000000",
+        background: theme !== "dark" ?  "#ffffff" : "#000000",
+    }
+
+    const sizeVariants = {
+        extraSmall: 5,
+        small: 8,
+        medium: 10,
+        large: 12,
+        extraLarge: 15
+    }
+
+    return (
+        <div className='flex items-center justify-center flex-col gap-4'>
+            <SyncLoader
+                color={colorVariants[color]}
+                size={sizeVariants[size]}
+                speedMultiplier={0.75}
+            />
+            <h1>{label}</h1>
+        </div>
+    )
+}
