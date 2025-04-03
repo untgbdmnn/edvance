0b1505a0db9fd6bb0fedf6c3a333ef6f703f5a39
Untung Budiman <untungbudiman.dev@gmail.com>
2025-04-03 18:08:18 +0700

    handle sign in and sigup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,14 +4,21 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import AuthLayout from '@/layouts/auth-layout'
+import useAuthStore from '@/lib/authStore'
+import fetchData from '@/lib/fetchData'
 import { SetTitle } from '@/lib/setHelmet'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import * as React from 'react'
+import { toast } from 'sonner'
 
 export default function LoginPage() {
+  const router = useRouter()
+  const { setAuth } = useAuthStore()
   const [state, setState] = React.useState({
     email: '',
-    password: ''
+    password: '',
+    loading: false
   })
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,7 +30,33 @@ export default function LoginPage() {
   }
 
   const handleSubmit = async () => {
-    console.log(state)
+    setState(prev => ({ ...prev, loading: true }))
+    const { email, password } = state
+    if (email === '' || password === '') {
+      toast.warning('Gagal!', {
+        description: 'Email dan password tidak boleh kosong!'
+      })
+      setState(prev => ({ ...prev, loading: false }))
+      return
+    }
+    const body = {
+      email,
+      password
+    }
+    const response = await fetchData.POST('auth/login', body)
+    if (response.status === 200) {
+      const { token, data } = response;
+      setAuth(token, data)
+      toast.success('Berhasil!', {
+        description: response.message
+      })
+      router.push('/dashboard')
+    } else {
+      toast.error('Gagal!', {
+        description: response.message
+      })
+    }
+    setState(prev => ({ ...prev, loading: false }))
   }
 
   return (
@@ -33,15 +66,19 @@ export default function LoginPage() {
         <div className='space-y-4'>
           <div className='form-group'>
             <Label htmlFor='email'>Email</Label>
-            <Input type='email' name='email' id='email' value={state.email} placeholder='Ketikan Email' autoComplete='off' onChange={handleChange}/>
+            <Input type='email' name='email' id='email' value={state.email} placeholder='Ketikan Email' autoComplete='off' onChange={handleChange} />
           </div>
           <div className='form-group'>
             <Label htmlFor='password'>Password</Label>
-            <Input type='password' name='password' id='password' value={state.password} autoComplete='off' onChange={handleChange}/>
-            <span className='text-xs'>Password minimum 8 karakter</span>
+            <Input type='password' name='password' id='password' value={state.password} autoComplete='off' onChange={handleChange} onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSubmit()
+              }
+            }} />
+            <span className='text-xs text-muted-foreground'>Password minimum 8 karakter</span>
           </div>
           <div className='w-full flex items-center justify-center flex-col gap-2'>
-            <Button type='button' onClick={handleSubmit} className='w-full cursor-pointer font-semibold'>Login</Button>
+            <Button type='button' onClick={handleSubmit} disabled={state.loading} className='w-full cursor-pointer font-semibold'>Login</Button>
             <span className='text-sm font-normal text-center'>Belum punya akun? <Link href='/sign-up' className='text-sm font-bold text-blue-500'>Register disini!</Link></span>
           </div>
         </div>
--- servers/controllers/authController.ts
+++ servers/controllers/authController.ts
@@ -1,6 +1,6 @@
 import parseRequest from "@/resources/helpers/parseRequest";
 import { Hono } from "hono";
-import { SignUpRequest } from "../models/authModel";
+import { SignInRquest, SignUpRequest } from "../models/authModel";
 import { AuthServices } from "../services/authServices";
 
 export const authController = new Hono()
@@ -8,5 +8,11 @@ export const authController = new Hono()
 authController.post('/register', async (c) => {
     const request = await parseRequest<SignUpRequest>(c)
     const response = await AuthServices.REGISTER(request)
+    return c.json(response)
+})
+
+authController.post('/login', async (c) => {
+    const request = await parseRequest<SignInRquest>(c)
+    const response = await AuthServices.LOGIN(request)
     return c.json(response)
 })
